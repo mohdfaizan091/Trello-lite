ad1022d050add6bf1f3d09a8f17c73d19f2f3063
MOHD FAIZAN <mohdfaiz0921078@gmail.com>
2026-07-27 01:53:47 +0530

    feat: add get route to get all the list

--- server/src/controllers/todoController.js
+++ server/src/controllers/todoController.js
@@ -1,6 +1,8 @@
 import Todo from '../model/list.js'; 
 
-export const createTodo = async (req, res) => {
+
+// add task
+const createTodo = async (req, res) => {
     try {
         const {title, description} = req.body;
         if(!title) {
@@ -22,3 +24,18 @@ export const createTodo = async (req, res) => {
         });
     }
 }
+
+// get all the task
+const getTodo = async (req, res) => {
+    try {
+        const todos = await Todo.find();
+        res.status(200).json(todos)
+    } catch(error) {
+        res.status(500).json({
+            message : "server error, could not fetch list",
+            error : error.message 
+        });
+    }
+};
+
+export { createTodo, getTodo }
--- server/src/routes/todoRoutes.js
+++ server/src/routes/todoRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 const router = express.Router();
-import {createTodo} from '../controllers/todoController.js'; 
+import {createTodo, getTodo} from '../controllers/todoController.js'; 
 
 router.post('/tasks', createTodo);
+router.get('/tasks', getTodo);
 
 export default router;
